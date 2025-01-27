efd612a04f74821d9797440590bfc7b82faf0089
TrapTheOnly <ismail.eyyub@gmail.com>
2025-01-27 14:48:13 +0400

    Refactor LDAP authentication to remove server reachability check and improve connection handling

--- backend/ldap_auth.py
+++ backend/ldap_auth.py
@@ -14,11 +14,9 @@ def ldap_authenticate(username, password):
     try:
         user_dn = f"{LDAP_DOMAIN}\\{username}"
         server = Server(LDAP_SERVER, get_info=ALL)
-        response = os.system(f"nc -z -w 5 {LDAP_SERVER} 389")
-        if response != 0:
-            raise Exception(f"Cannot reach LDAP server: {LDAP_SERVER}")
-        else:
-            print("LDAP server is up!")
+        print(server)
+        conn1 = Connection(server, auto_bind=True)
+        print(conn1)
         conn = Connection(server, user=user_dn, password=password, authentication=NTLM, auto_bind=True)
         print(conn)
         conn.unbind()
