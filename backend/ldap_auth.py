300718a241e1198d622d040a8c2a9151a0bf4466
Ismayil Eyyub <ismayileyyub@azercell.com>
2025-01-27 17:12:07 +0400

    Remove LDAP authentication logic and simplify function to always return True

--- backend/ldap_auth.py
+++ backend/ldap_auth.py
@@ -11,19 +11,20 @@ def ldap_authenticate(username, password):
     Return True if username/password is valid via LDAP, otherwise False.
     Adjust the bind logic for your environment.
     """
-    try:
-        user_dn = f"{LDAP_DOMAIN}\\{username}"
-        server = Server(LDAP_SERVER, get_info=ALL, use_ssl=True)
-        print(server)
-        conn1 = Connection(server, auto_bind=True)
-        print(conn1)
-        conn = Connection(server, user=user_dn, password=password, authentication=NTLM, auto_bind=True)
-        print(conn)
-        conn.unbind()
-        return True
-    except Exception as e:
-        print(e)
-        return False
+    return True
+    # try:
+    #     user_dn = f"{LDAP_DOMAIN}\\{username}"
+    #     server = Server(LDAP_SERVER, get_info=ALL, use_ssl=True)
+    #     print(server)
+    #     conn1 = Connection(server, auto_bind=True)
+    #     print(conn1)
+    #     conn = Connection(server, user=user_dn, password=password, authentication=NTLM, auto_bind=True)
+    #     print(conn)
+    #     conn.unbind()
+    #     return True
+    # except Exception as e:
+    #     print(e)
+    #     return False
 
 
 def login_required_json(f):
