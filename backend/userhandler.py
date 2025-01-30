eb7e0077d87d313505d429d87248eb544accee8c
Ismayil Eyyub <ismayileyyub@azercell.com>
2025-01-30 11:08:42 +0400

    Fix LDAP search filter to include wildcard for query matching

--- backend/userhandler.py
+++ backend/userhandler.py
@@ -47,7 +47,7 @@ def search_ldap_users(query, page_size=500):
         logger.info(f"LDAP search query: {query}")
 
         # Define search filter and attributes
-        search_filter = f"(|(sAMAccountName=*{query})(mail=*{query}))"
+        search_filter = f"(|(sAMAccountName=*{query}*)(mail=*{query}*))"
         attributes = ['name', 'sAMAccountName', 'distinguishedName', 'mail']
 
         # Initialize results and pagination variables
