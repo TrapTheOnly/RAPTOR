0ba4f6cfabd60e536301304c8115166af0a5a0e6
Ismayil Eyyub <ismayileyyub@azercell.com>
2025-02-27 17:09:42 +0400

    Refactor create_or_update_pentest_data to streamline existing data retrieval and enhance logging

--- backend/modules/offsec.py
+++ backend/modules/offsec.py
@@ -143,9 +143,8 @@ def create_or_update_pentest_data(record_id):
         c = conn.cursor()
         c.execute("SELECT * FROM pentest_data")
         rows = c.fetchall()
-        existing_data = [dict(ix) for ix in rows if ix['record_id'] == record_id]
-        existing_data = existing_data if existing_data else {}
-        print(existing_data)
+        existing_data = [dict(ix) for ix in rows if ix['record_id'] == record_id][0] or {}
+        logger.debug(existing_data)
         data = {}
         for key in ['vulnerable', 'tested_by', 'test_start_date', 'test_end_date', 'vulnerability_fixed', 'service_desk_link', 'status']:
             if (value := request.form.get(key)) is not None:
